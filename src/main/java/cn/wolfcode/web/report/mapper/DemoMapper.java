3f96971f71bc214a3e576f5be05a26136562251f
xzs-Rabble <114422586+xzs-Rabble@users.noreply.github.com>
2022-10-08 22:42:05 +0800

    Delete src/main/java/cn/wolfcode/web directory

--- src/main/java/cn/wolfcode/web/report/mapper/DemoMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.wolfcode.web.modules.report.mapper;
-
-import cn.wolfcode.web.modules.report.entity.Demo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-
-public interface DemoMapper extends BaseMapper<Demo> {
-
-}
